70af3417831e584429cd1af1c58922efe3e224a2
rizki12-cyber <rizqiapriana@gmail.com>
2025-11-13 14:21:49 +0700

    broo

--- resources/views/admin/konsentrasi/edit.blade.php
+++ resources/views/admin/konsentrasi/edit.blade.php
@@ -6,7 +6,7 @@
 <div class="container py-4">
     <h3 class="mb-4">Edit Konsentrasi Keahlian</h3>
 
-    <form action="{{ route('konsentrasi.update', $konsentrasi->id) }}" method="POST">
+    <form action="{{ route('admin.konsentrasi.update', $konsentrasi->id) }}" method="POST">
         @csrf
         @method('PUT')
 
@@ -27,7 +27,7 @@
         </div>
 
         <button type="submit" class="btn btn-primary">Update</button>
-        <a href="{{ route('konsentrasi.index') }}" class="btn btn-secondary">Kembali</a>
+        <a href="{{ route('admin.konsentrasi.index') }}" class="btn btn-secondary">Kembali</a>
     </form>
 </div>
 @endsection
